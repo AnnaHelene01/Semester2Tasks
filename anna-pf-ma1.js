d852fb84ba45718e82aa3fe8fc674fec036819e6
AnnaHelene01 <annahelene2001@gmail.com>
2022-01-09 22:20:58 +0100

    ddd

---
+++ anna-pf-ma1.js
@@ -0,0 +1,34 @@
+// Module Assignment 1 
+// Question 1
+var age = 20; //Number
+var firstName = "Anna"; //String
+var x = true; // Boolean
+console.log(age);
+console.log(firstName);
+console.log(x);
+
+
+// Question 2
+var firstName = "Anna";
+var lastName = "Sæthre";
+var fullName = firstName + " " + lastName; 
+console.log(fullName);
+
+// Question 3
+var animal = "frog";
+console.log("The type frog is string");
+
+// Question 4
+var orderHasShipped = true; // Boolean value
+if (orderHasShipped === true) {
+    console.log("The order is shippped")
+}
+
+else {
+    console.log("The order is not shippped");
+}
+
+// Question 5
+for (var i = 7; i <= 13; i++) {
+    console.log(i);
+}
